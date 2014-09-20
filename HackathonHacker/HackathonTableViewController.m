e2673005ac62b683775f4bd9ceb0e8dce420bd4e
gailees <gailees@umich.edu>
2014-09-20 06:21:53 -0400

    ignoring xcuserdata/

--- HackathonHacker/HackathonTableViewController.m
+++ HackathonHacker/HackathonTableViewController.m
@@ -27,7 +27,9 @@
 {
     [super viewDidLoad];
     
-    self.hackathons = [NSArray arrayWithObjects:@"MHacks", @"PennApps", @"HackTheNorth", @"YHacks", nil];
+    self.hackathons = @[@"MHacks", @"PennApps", @"HackTheNorth", @"YHacks"];
+    
+    self.hackathonsArray = [NSMutableArray new];
     
     self.hackathonsArray[0] = self.hackathons;
     self.hackathonsArray[1] = [NSArray arrayWithObjects:@"hackNY", @"HackGT", @"CalHacks", @"HackTX", @"BoilerMake", nil];
@@ -52,6 +54,7 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
     // Return the number of sections.
+    //return 1;
     return 3;
 }
 
@@ -59,6 +62,9 @@
 {
     // Return the number of rows in the section.
     
+    //return [[self.hackathonsArray objectAtIndex:section] count];
+    NSLog(@"%lu", (unsigned long)section);
+    NSLog(@"%lu", (unsigned long)[[self.hackathonsArray objectAtIndex:section] count]);
     return [[self.hackathonsArray objectAtIndex:section] count];
 }
 
@@ -67,34 +73,39 @@
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     // Configure the cell...
-    cell.textLabel.text = [self.hackathons objectAtIndex:indexPath.row];
+//    cell.textLabel.text = [self.hackathons objectAtIndex:indexPath.row];
+    //NSLog(@"%@", [self.hackathonsArray objectAtIndex:0]);
+    cell.textLabel.text = [[self.hackathonsArray objectAtIndex:indexPath.section]objectAtIndex:indexPath.row];
+    
+    //cell.textLabel.text = [self.hackathons objectAtIndex:indexPath.row];
     
     //cell.textLabel.text = [self.hackathonsArray[0] objectAtIndex:indexPath.row];
-    NSLog(@"%@", cell.textLabel.text);
+    //NSLog(@"%@", cell.textLabel.text);
     //instead check hackathon date and determine
-    
     /*
-    switch (indexPath.row) {
+    self.hackathons = [self.hackathonsArray objectAtIndex:indexPath.section];
+    switch (indexPath.section) {
         case 0:
             // Configure the cell...
             cell.textLabel.text = [self.hackathons objectAtIndex:indexPath.row];
             break;
         case 1:
             // Configure the cell...
-            cell.textLabel.text = [self.hackathons1 objectAtIndex:indexPath.row];
+            cell.textLabel.text = [self.hackathons objectAtIndex:indexPath.row];
             break;
         case 2:
             // Configure the cell...
-            cell.textLabel.text = [self.hackathons2 objectAtIndex:indexPath.row];
+            cell.textLabel.text = [self.hackathons objectAtIndex:indexPath.row];
             break;
         case 3:
             // Configure the cell...
-            cell.textLabel.text = [self.hackathons3 objectAtIndex:indexPath.row];
+            cell.textLabel.text = [self.hackathons objectAtIndex:indexPath.row];
             break;
         default:
             break;
     }
      */
+    
     
     return cell;
 }
